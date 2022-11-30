79081874d9893ce535f52dc2b6896b5a4732475a
Jeffrey Hu <jeffreyhu98@gmail.com>
2022-11-29 20:58:27 -0800

    added: notes, attr to agent(aoe, delay) rem: wall

--- agent.py
+++ agent.py
@@ -2,7 +2,6 @@ class Agent:
     def __init__(self, name):
         self.name = name
         self.smoke = 0
-        self.wall = 0
         self.trip = 0
         self.reconSoft = 0
         self.reconHard = 0
@@ -11,3 +10,5 @@ class Agent:
         self.nearsight = 0
         self.stun = 0
         self.operator = 0
+        self.aoe = 0                    #nade, molly, sieze, pull
+        self.delay = 0                  #slow, sagewall, pull
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from omen import Omen
 
+'''
+ideas:
+    -recs based on map
+        ex: most likely to play operator here
+            becareful going hookah (your team has no drone)
+    -composition overview (1 controller, 2 initiator, 1 duelist, 1 controller, etc)
+    -recs based on agents
+'''
+
+
 def test_omen():
     player = Omen()
     print("[",player.smoke,"]", "expected: 1")
     print("[",player.drone,"]", "expected: 0")
-    print("[",player.wall,"]", "expected: 0")
     print("[",player.trip,"]", "expected: 0")
     print("[",player.reconSoft,"]", "expected: 0")
     print("[",player.reconHard,"]", "expected: 0")
